46cd179f6628914a5d016fd59d4e26600cb4d5a9
adeebkhans <mdadeebkhan01@gmail.com>
2025-06-04 14:43:14 +0530

    intital code

---
+++ src/resolvers/index.js
@@ -0,0 +1,12 @@
+import Resolver from '@forge/resolver';
+
+const resolver = new Resolver();
+
+resolver.define('getText', (req) => {
+  console.log(req);
+
+  return 'Welcome to the ' + req.context.extension.space.key + ' space!';
+
+});
+
+export const handler = resolver.getDefinitions();
